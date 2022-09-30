d7dceb2aa794bd1aeae643183147b5ec2a3f5ec0
Slmpire <sioplightman5@gmail.com>
2022-09-30 13:24:13 -0700

    Task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - function
+ *@argc: parameter
+ *@argv: parameter
+ *Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
